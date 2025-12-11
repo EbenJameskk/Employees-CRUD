dd2bc86eea08d934ece9034c20def013986af6b3
Sawer <sawerebenezer@gmail.com>
2025-12-11 02:21:37 -0800

    Update

--- Employe/urls.py
+++ Employe/urls.py
@@ -12,7 +12,7 @@
 
 from django.urls import path
 
-from employee_details import views
+from Employe import views
 
 urlpatterns = [
     path('', views.employee_list, name='employee_list'),
--- Employe/views.py
+++ Employe/views.py
@@ -1,4 +1,11 @@
-
+# from django.shortcuts import render
+# from django.views.decorators.csrf import csrf_exempt
+# # Create your views here.
+# from django.http import HttpResponse
+# from django.views.generic import ListView, DetailView
+# from django.views.generic.edit import CreateView, UpdateView, DeleteView
+# from django.urls import reverse_lazy
+# from .models import Employee
 
 from django.shortcuts import render, redirect, get_object_or_404
 from django.forms import ModelForm
@@ -22,9 +29,7 @@ def employee_list(request, template_name='employee_details/employee_list.html'):
 
 def employee_view(request, pk, template_name='employee_details/employee_detail.html'):
     employee= get_object_or_404(Employee, pk=pk)
-    return render(request, template_name, {'object':employee})
-
-
+    return render(request, template_name, {'object':employee}) 
 def employee_create(request, template_name='employee_details/employee_form.html'):
     form = EmployeeForm(request.POST or None)
     if form.is_valid():
